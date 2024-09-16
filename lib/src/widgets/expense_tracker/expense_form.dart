d270774d292ebc90df507211a6e5ca0aa45abcb7
RJ Villanueva <rj.villanueva89@gmail.com>
2024-09-16 17:01:10 +0800

    update ExpenseForm, used TextEditingController and added showDatePicker

--- lib/src/widgets/expense_tracker/expense_form.dart
+++ lib/src/widgets/expense_tracker/expense_form.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/src/models/expense_data.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseForm extends StatefulWidget {
@@ -10,20 +11,40 @@ class ExpenseForm extends StatefulWidget {
 }
 
 class _ExpenseFormState extends State<ExpenseForm> {
-  Map<String, Object> formValues = {};
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _createdAt;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void _openDatePicker() async {
+    final now = DateTime.now();
+    final minDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: minDate,
+      lastDate: now,
+    );
+
+    setState(() {
+      _createdAt = pickedDate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    void saveTitle(String value) {
-      formValues["label"] = value;
-    }
-
     return Padding(
       padding: const EdgeInsets.all(15),
       child: Column(
         children: [
           TextField(
-            onChanged: saveTitle,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text("Label"),
@@ -31,9 +52,51 @@ class _ExpenseFormState extends State<ExpenseForm> {
           ),
           Row(
             children: [
-              ElevatedButton(
+              Expanded(
+                  child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  prefixText: '\$ ',
+                  label: Text("Amount"),
+                ),
+              )),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_createdAt != null
+                        ? formatter.format(_createdAt!)
+                        : "Select a date"),
+                    IconButton(
+                      onPressed: _openDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
                 onPressed: () {
-                  print(formValues);
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+              const SizedBox(width: 15),
+              FilledButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                  print(_createdAt != null
+                      ? formatter.format(_createdAt!)
+                      : "No date selected");
                 },
                 child: const Text("Save"),
               )
